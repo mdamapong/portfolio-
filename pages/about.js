7d379d164f3963220bc22bbef41f88108ccd9471
mickey <damapongm@gmail.com>
2022-04-24 15:58:40 -0700

    sec commit

--- pages/about.js
+++ pages/about.js
@@ -7,10 +7,9 @@ export default function about() {
 				<h1>About</h1>
 				<p>
 					This is A portfolio profile for Mickey Damapong. I'm a new devoloper
-					who strive to learn, create, fix, and improve…I will share my
-					knowledge, skills, and seek to learn more.
+					who strive to learn create fix and improve I will share my knowledge
+					skills and seek to learn more.
 				</p>
-				<p></p>
 			</div>
 		</>
 	);
